2ba53a9f7fc3826d2832f3b685c46b59544dfda8
Dan Phan <danh.phan.mq@gmail.com>
2022-08-02 17:19:05 +1000

    add calc

---
+++ calc.py
@@ -0,0 +1,17 @@
+# This perfect calculation is written by Alice
+# Perform simple arithmetic encoded in an input string:
+# '1 + 2' -> 3, or '1 - 2' -> -1.
+def compute(expression):
+    num0, operator, num1 = expression.split(' ')
+    num0, num1 = int(num0), int(num1)
+    if operator == '+':
+        return num0 + num1
+    elif operator == '-':
+        return num0 - num1
+    elif operator == '*':
+        return num0 * num1
+    elif operator == '/':
+        return num0 / num1
+    else:
+        print('unknown operator!')
+        return None
